c99f51553d0a5d5b21eaca333d5f7afb56777710
TouchyPunchy <julien.vigneron@gmail.com>
2016-11-24 01:28:47 +0100

    Updated canvas size

--- public/js/ballsadventure01.js
+++ public/js/ballsadventure01.js
@@ -1,5 +1,5 @@
 // -----------------------
-// 101_game.js 
+// ballsadventure01.js 
 //
 // using : 
 // p5.js + pxgamepad.js
@@ -17,6 +17,8 @@ var gameManager;
 function setup(){
 	W = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	H = window.innerHeight || document.documentElement.clientHeight	|| document.body.clientHeight;
+	W = W - 32;
+	H = H - 18;
 	createCanvas(W,H);
 	gameManager = new GameManager(0);
 }
